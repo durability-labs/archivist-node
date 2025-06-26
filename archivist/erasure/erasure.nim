791b0cd33a34848eaaa4ddd70e632670a260bbb3
Mark Spanbroek <mark@spanbroek.net>
2025-06-26 11:48:06 +0200

    chore: formatting

--- archivist/erasure/erasure.nim
+++ archivist/erasure/erasure.nim
@@ -514,7 +514,7 @@ proc asyncDecode*(
 
 proc decodeInternal(
     self: Erasure, encoded: Manifest
-): Future[?!(ref seq[Cid], seq[Natural])] {.async: (raises:[CancelledError]).} =
+): Future[?!(ref seq[Cid], seq[Natural])] {.async: (raises: [CancelledError]).} =
   logScope:
     steps = encoded.steps
     rounded_blocks = encoded.rounded
