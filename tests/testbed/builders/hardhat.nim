b271d9445c9d10cf3883f70453fe531c6fb0e5e9
Mark Spanbroek <mark@spanbroek.net>
2025-08-26 10:54:54 +0200

    feat(testbed): testbed with hardhat and node builders

---
+++ tests/testbed/builders/hardhat.nim
@@ -0,0 +1,17 @@
+import pkg/chronos
+import pkg/questionable
+import ../testbed
+import ../hardhat
+
+type HardhatBuilder = ref object
+  testbed: Testbed
+
+func hardhat*(testbed: Testbed): HardhatBuilder =
+  HardhatBuilder(testbed: testbed)
+
+proc start*(builder: HardhatBuilder): Future[Hardhat] {.async.} =
+  if builder.testbed.hardhatInstance.isSome:
+    raise newException(TestbedError, "hardhat already started")
+  let hardhat = await Hardhat.start()
+  builder.testbed.hardhatInstance = some hardhat
+  hardhat
