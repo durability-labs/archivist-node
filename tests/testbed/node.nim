c50c001262a44d7a4d9c1a8c24acec1810674fa5
Mark Spanbroek <mark@spanbroek.net>
2025-08-26 15:24:27 +0200

    feat(testbed): add api url to node helper

--- tests/testbed/node.nim
+++ tests/testbed/node.nim
@@ -1,9 +1,18 @@
 import std/os
+import std/net
 import pkg/chronos
+import pkg/questionable
 import ./process
 
 type Node* = ref object
   process: Process
+  apiAddress: ?IpAddress
+  apiPort: ?Port
+
+func apiUrl*(node: Node): string =
+  let address = node.apiAddress |? static parseIpAddress("127.0.0.1")
+  let port = node.apiPort |? Port(8080)
+  "http://" & $address & ":" & $port & "/api/archivist/v1"
 
 const projectRoot = currentSourcePath().parentDir().parentDir().parentDir()
 const buildDir = projectRoot / "build"
