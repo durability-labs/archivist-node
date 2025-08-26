3b2cac40a7578959c6aa09301ea4336640cd3aa5
Mark Spanbroek <mark@spanbroek.net>
2025-08-26 09:39:53 +0200

    feat(testbed): http and process helpers

---
+++ tests/testbed/http.nim
@@ -0,0 +1,51 @@
+import pkg/chronos/apps/http/httpclient
+import pkg/stew/byteutils
+
+type Http* = object
+
+type HttpHeaders* = seq[(string, string)]
+
+type
+  HttpRequestOptions* = set[HttpRequestOption]
+  HttpRequestOption* {.pure.} = enum
+    checkStatusCode
+
+type HttpResponse* = distinct HttpClientResponseRef
+
+proc post*(
+  _: type Http,
+  url: string,
+  body: seq[byte],
+  headers: HttpHeaders = @{:},
+  options: HttpRequestOptions = {HttpRequestOption.checkStatusCode}
+): Future[HttpResponse] {.async.} =
+  let session = HttpSessionRef.new()
+
+  let request =
+    HttpClientRequestRef
+      .post(session, url, body = body, headers = headers)
+      .tryGet()
+
+  var response: HttpResponse
+  try:
+    let response = HttpResponse(await request.send())
+  except HttpError as error:
+    await noCancel session.closeWait()
+    raise error
+
+  if HttpRequestOption.checkStatusCode in options:
+    if response.status < 200 or response.status >= 300:
+      raise newException(HttpError, "HTTP status code: " & response.status)
+
+  response
+
+proc close*(response: HttpResponse) {.async.} =
+  await noCancel HttpClientResponseRef(response).session.closeWait()
+
+proc readBody*(response: HttpResponse): Future[seq[byte]] {.async.} =
+  let body  = await HttpClientResponseRef(response).getBodyBytes()
+  await response.close()
+  body
+
+proc readBodyString*(response: HttpResponse): Future[string] {.async.} =
+  string.fromBytes(await response.readBody())
