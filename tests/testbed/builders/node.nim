174394f34e4a59c01a5de4a21c5dce51e28087ac
Mark Spanbroek <mark@spanbroek.net>
2025-08-28 11:57:45 +0200

    feat(testbed): add persistence support to node

--- tests/testbed/builders/node.nim
+++ tests/testbed/builders/node.nim
@@ -1,14 +1,47 @@
 import pkg/chronos
+import pkg/questionable
 import ../testbed
 import ../node
+import ../hardhat
 
-type NodeBuilder = ref object
-  testbed: Testbed
+type
+  NodeBuilder = ref object
+    testbed: Testbed
+    persistence: ? bool
+    ethPrivateKey: ? ? string
 
 func node*(testbed: Testbed): NodeBuilder =
   NodeBuilder(testbed: testbed)
 
+func persistence*(builder: NodeBuilder, enabled: bool = true): NodeBuilder =
+  builder.persistence = some enabled
+  builder
+
+func ethPrivateKey*(builder: NodeBuilder, filename: string): NodeBuilder =
+  builder.ethPrivateKey = some some filename
+  builder
+
+func noEthPrivateKey*(builder: NodeBuilder): NodeBuilder =
+  builder.ethPrivateKey = some none string
+  builder
+
+func persistenceResolved(builder: NodeBuilder): bool =
+  builder.persistence |? true
+
+func ethPrivateKeyResolved(builder: NodeBuilder): ?string =
+  if ethPrivateKey =? builder.ethPrivateKey:
+    return ethPrivateKey
+  if builder.persistenceResolved:
+    if hardhat =? builder.testbed.hardhatInstance:
+      return some hardhat.accounts.pop().privateKeyFile
+  none string
+
 proc start*(builder: NodeBuilder): Future[Node] {.async.} =
-  let node = await Node.start(@[])
+  var arguments: seq[string]
+  if builder.persistenceResolved:
+    arguments.add("persistence")
+  if ethPrivateKey =? builder.ethPrivateKeyResolved:
+    arguments.add("--eth-private-key=" & ethPrivateKey)
+  let node = await Node.start(arguments)
   builder.testbed.nodeInstances.add(node)
   node
