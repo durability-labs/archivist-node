405da056ba4d7d895aac74000fc5a7b7897b4eb9
Mark Spanbroek <mark@spanbroek.net>
2025-08-29 15:32:38 +0200

    feat(testbed): wait for more events

--- tests/testbed/network/contract.nim
+++ tests/testbed/network/contract.nim
@@ -4,3 +4,10 @@ type MarketplaceContract* = ref object of Contract
 
 type RequestFulfilled* = object of Event
   requestId* {.indexed.}: array[32, byte]
+
+type ProofSubmitted* = object of Event
+  id*: array[32, byte]
+
+type SlotFreed* = object of Event
+  requestId* {.indexed.}: array[32, byte]
+  slotIndex*: uint64
