b9dbd20f69cb75dc6fe741860825eddd2dc59e64
Mark Spanbroek <mark@spanbroek.net>
2025-09-01 14:20:00 +0200

    feat(testbed): find unused udp port for discovery

--- tests/testbed/helpers/ports.nim
+++ tests/testbed/helpers/ports.nim
@@ -1,16 +1,35 @@
 import std/net
 import pkg/chronos
 
-proc isPortFree*(address: IpAddress, port: Port): Future[bool] {.async.} =
+type Protocol* = enum
+  Tcp
+  Udp
+
+proc isPortFree*(
+  address: IpAddress,
+  port: Port,
+  protocol: Protocol
+): Future[bool] {.async.} =
   try:
     let address = initTAddress(address, port)
-    let server = createStreamServer(address, {ReuseAddr})
-    await server.closeWait()
+    case protocol
+    of Tcp:
+      let server = createStreamServer(address, {ReuseAddr})
+      await server.closeWait()
+    of Udp:
+      proc callback(_: DatagramTransport, _: TransportAddress) {.async:(raises:[]).} =
+        discard
+      let server = newDatagramTransport(callback, local = address, flags = {ReuseAddr})
+      await server.closeWait()
     true
   except TransportOsError:
     false
 
-proc findFreePort*(address: IpAddress, start: Port): Future[Port] {.async.} =
+proc findFreePort*(
+  address: IpAddress,
+  start: Port,
+  protocol = Tcp
+): Future[Port] {.async.} =
   result = start
-  while not await isPortFree(address, result):
+  while not await isPortFree(address, result, protocol):
     inc result
