21bd14729260ba45606c250a5da547df813d9055
Mark Spanbroek <mark@spanbroek.net>
2025-09-02 11:55:05 +0200

    feat(testbed): advance time

---
+++ tests/testbed/builders/time.nim
@@ -0,0 +1,15 @@
+import std/strutils
+import pkg/chronos
+import pkg/ethers
+import ../testbed
+
+type TimeBuilder = ref object
+  testbed: Testbed
+
+func time*(testbed: Testbed): TimeBuilder =
+  TimeBuilder(testbed: testbed)
+
+proc advance*(builder: TimeBuilder, seconds: int) {.async.} =
+  let provider = builder.testbed.provider
+  discard await provider.send("evm_increaseTime", @[%("0x" & seconds.toHex)])
+  discard await provider.send("evm_mine")
