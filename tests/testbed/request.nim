6424517a9e138dc4d7e0c9ea6f44aa4ffb3182f1
Mark Spanbroek <mark@spanbroek.net>
2025-09-02 13:21:34 +0200

    feat(testbed): keep track of duration and expiry for request

--- tests/testbed/request.nim
+++ tests/testbed/request.nim
@@ -3,12 +3,26 @@ import ./dataset
 type Request* = ref object
   dataset: Dataset
   id: string
+  duration: int
+  expiry: int
 
-func init*(_: type Request, dataset: Dataset, id: string): Request =
-  Request(dataset: dataset, id: id)
+func init*(
+  _: type Request,
+  dataset: Dataset,
+  id: string,
+  duration: int,
+  expiry: int
+): Request =
+  Request(dataset: dataset, id: id, duration: duration, expiry: expiry)
 
 func dataset*(request: Request): Dataset =
   request.dataset
 
 func id*(request: Request): string =
   request.id
+
+func duration*(request: Request): int =
+  request.duration
+
+func expiry*(request: Request): int =
+  request.expiry
