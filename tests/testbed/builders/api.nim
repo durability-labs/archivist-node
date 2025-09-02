d2f6a14f756eeb9e4cb324e66848ff22ada501cf
Mark Spanbroek <mark@spanbroek.net>
2025-09-02 11:52:32 +0200

    feat(testbed): ask node for its eth address

--- tests/testbed/builders/api.nim
+++ tests/testbed/builders/api.nim
@@ -1,5 +1,6 @@
 import std/json
 import pkg/chronos
+import pkg/ethers
 import pkg/questionable
 import ../testbed
 import ../network/node
@@ -12,12 +13,22 @@ type ApiBuilder = ref object
 func api*(testbed: Testbed, node: Node): ApiBuilder =
   ApiBuilder(testbed: testbed, url: node.apiUrl)
 
+proc getInfo(builder: ApiBuilder): Future[JsonNode] {.async.} =
+  await Http.get(builder.url & "/debug/info").readJson()
+
 proc getSpr*(builder: ApiBuilder): Future[?string] {.async.} =
-  let info = await Http.get(builder.url & "/debug/info").readJson()
+  let info = await builder.getInfo()
   if spr =? info{"spr"}:
     some spr.getStr()
   else:
     none string
+
+proc getEthAddress*(builder: ApiBuilder): Future[?Address] {.async.} =
+  let info = await builder.getInfo()
+  if address =? info{"ethAddress"}:
+    Address.init(address.getStr())
+  else:
+    none Address
 
 proc getPurchase*(builder: ApiBuilder, id: string): Future[JsonNode] {.async.} =
   await Http.get(builder.url & "/storage/purchases/" & id).readJson()
