114a85c2f99bd345b40d0f719ca606aea08c0425
Mark Spanbroek <mark@spanbroek.net>
2025-09-15 13:08:14 +0200

    refactor(integration): use testbed for sales test

--- tests/integration/5_minutes/testsales.nim
+++ tests/integration/5_minutes/testsales.nim
@@ -1,233 +1,94 @@
-import std/httpclient
-import std/times
-import pkg/archivist/contracts
-from pkg/archivist/stores/repostore/types import DefaultQuotaBytes
-import ../twonodes
-import ../../archivist/examples
-import ../../contracts/time
-import ../archivistconfig
-import ../archivistclient
-import ../nodeconfigs
-import ../marketplacesuite
+import std/json
+import std/strutils
+import pkg/asynctest/chronos/unittest2
+import ../../testbed
 
-proc findItem[T](items: seq[T], item: T): ?!T =
-  for tmp in items:
-    if tmp == item:
-      return success tmp
+suite "Sales availability":
 
-  return failure("Not found")
-
-marketplacesuite(name = "Sales", stopOnRequestFail = true):
-  let salesConfig = NodeConfigs(
-    clients: ArchivistConfigs.init(nodes = 1).some,
-    providers: ArchivistConfigs.init(nodes = 1)
-    # .debug() # uncomment to enable console log output
-    # .withLogFile() # uncomment to output log file to tests/integration/logs/<start_datetime> <suite_name>/<test_name>/<node_role>_<node_idx>.log
-    # .withLogTopics("node", "marketplace", "sales", "reservations", "node", "proving", "clock")
-    .some,
-  )
-
-  var host: ArchivistClient
-  var client: ArchivistClient
+  var testbed: Testbed
+  var node, provider: Node
 
   setup:
-    host = providers()[0].client
-    client = clients()[0].client
+    testbed = await Testbed.start()
+    discard await testbed.hardhat.start()
+    node = await testbed.node.persistence.start()
+    provider = await testbed.node.provider.availability(false).start()
 
-  test "node handles new storage availability", salesConfig:
-    let availability1 = (
-      await host.postAvailability(
-        totalSize = 1.uint64,
-        duration = 2.uint64,
-        minPricePerBytePerSecond = 3.u256,
-        totalCollateral = 4.u256,
-      )
-    ).get
-    let availability2 = (
-      await host.postAvailability(
-        totalSize = 4.uint64,
-        duration = 5.uint64,
-        minPricePerBytePerSecond = 6.u256,
-        totalCollateral = 7.u256,
-      )
-    ).get
-    check availability1 != availability2
+  teardown:
+    await testbed.stop()
 
-  test "node lists storage that is for sale", salesConfig:
-    let availability = (
-      await host.postAvailability(
-        totalSize = 1.uint64,
-        duration = 2.uint64,
-        minPricePerBytePerSecond = 3.u256,
-        totalCollateral = 4.u256,
-      )
-    ).get
-    check availability in (await host.getAvailabilities()).get
+  test "node handles new storage availability":
+    let availability1 = await testbed.availability.create(provider)
+    let availability2 = await testbed.availability.create(provider)
+    check availability1{"id"} != availability2{"id"}
 
-  test "updating availability", salesConfig:
-    let availability = (
-      await host.postAvailability(
-        totalSize = 140000.uint64,
-        duration = 200.uint64,
-        minPricePerBytePerSecond = 3.u256,
-        totalCollateral = 300.u256,
-      )
-    ).get
+  test "node lists storage that is for sale":
+    let availability = await testbed.availability.create(provider)
+    check availability in await testbed.api(provider).getAvailability()
 
-    var until = getTime().toUnix()
+  test "node handles availability updates":
+    let original = await testbed.availability.create(provider)
+    let id = original["id"].getStr()
+    let until = (await testbed.time.now()) + 100
+    await testbed
+      .availability
+      .duration(100)
+      .minPricePerBytePerSecond(2)
+      .totalCollateral(200)
+      .enabled(false)
+      .until(until)
+      .update(provider, id)
+    let updated = (await testbed.api(provider).getAvailability())[0]
+    check updated{"duration"} == %100
+    check updated{"minPricePerBytePerSecond"} == %"2"
+    check updated{"totalCollateral"} == %"200"
+    check updated{"totalSize"} == original{"totalSize"}
+    check updated{"freeSize"} == original{"freeSize"}
+    check updated{"enabled"} == %false
+    check updated{"until"} == %until
 
-    await host.patchAvailability(
-      availability.id,
-      duration = 100.uint64.some,
-      minPricePerBytePerSecond = 2.u256.some,
-      totalCollateral = 200.u256.some,
-      enabled = false.some,
-      until = until.some,
-    )
+  test "node handles updates to totalSize":
+    let original = await testbed.availability.create(provider)
+    let id = original["id"].getStr()
+    await testbed.availability.totalSize(100).update(provider, id)
+    let updated = (await testbed.api(provider).getAvailability())[0]
+    check updated{"totalSize"} == %100
+    check updated{"freeSize"} == %100
 
-    let updatedAvailability =
-      ((await host.getAvailabilities()).get).findItem(availability).get
-    check updatedAvailability.duration == 100.uint64
-    check updatedAvailability.minPricePerBytePerSecond == 2
-    check updatedAvailability.totalCollateral == 200
-    check updatedAvailability.totalSize == 140000.uint64
-    check updatedAvailability.freeSize == 140000.uint64
-    check updatedAvailability.enabled == false
-    check updatedAvailability.until == until
+  test "node refuses a negative value for 'until'":
+    let original = await testbed.availability.create(provider)
+    let id = original["id"].getStr()
+    try:
+      await testbed.availability.until(-1).update(provider, id)
+      fail()
+    except HttpError as error:
+      check "422" in error.msg
+      check "Cannot set until to a negative value" in error.msg
 
-  test "updating availability - updating totalSize", salesConfig:
-    let availability = (
-      await host.postAvailability(
-        totalSize = 140000.uint64,
-        duration = 200.uint64,
-        minPricePerBytePerSecond = 3.u256,
-        totalCollateral = 300.u256,
-      )
-    ).get
-    await host.patchAvailability(availability.id, totalSize = 100000.uint64.some)
+  test "node refuses to decrease totalSize below amount used by slots":
+    let original = await testbed.availability.create(provider)
+    let id = original["id"].getStr()
+    discard await testbed.request.start(node)
+    let updated = (await testbed.api(provider).getAvailability())[0]
+    check updated["totalSize"].getInt() == original["totalSize"].getInt()
+    check updated["freeSize"].getInt() < original["freeSize"].getInt()
+    try:
+      let used = updated["totalSize"].getInt() - updated["freeSize"].getInt()
+      await testbed.availability.totalSize(used - 1).update(provider, id)
+      fail()
+    except HttpError as error:
+      check "422" in error.msg
+      check "must be larger then current totalSize - freeSize" in error.msg
 
-    let updatedAvailability =
-      ((await host.getAvailabilities()).get).findItem(availability).get
-    check updatedAvailability.totalSize == 100000
-    check updatedAvailability.freeSize == 100000
-
-  test "updating availability - updating totalSize does not allow bellow utilized",
-    salesConfig:
-    let originalSize = 0xFFFFFF.uint64
-    let data = await RandomChunker.example(blocks = 8)
-    let minPricePerBytePerSecond = 3.u256
-    let collateralPerByte = 1.u256
-    let totalCollateral = originalSize.u256 * collateralPerByte
-    let availability = (
-      await host.postAvailability(
-        totalSize = originalSize,
-        duration = 20 * 60.uint64,
-        minPricePerBytePerSecond = minPricePerBytePerSecond,
-        totalCollateral = totalCollateral,
-      )
-    ).get
-
-    # Lets create storage request that will utilize some of the availability's space
-    let cid = (await client.upload(data)).get
-    let id = await client.requestStorage(
-      cid,
-      duration = 20 * 60.uint64,
-      pricePerBytePerSecond = minPricePerBytePerSecond,
-      proofProbability = 3.u256,
-      expiry = (10 * 60).uint64,
-      collateralPerByte = collateralPerByte,
-      nodes = 3,
-      tolerance = 1,
-    )
-
-    discard await waitForRequestToStart()
-
-    let updatedAvailability =
-      ((await host.getAvailabilities()).get).findItem(availability).get
-    check updatedAvailability.totalSize != updatedAvailability.freeSize
-
-    let utilizedSize = updatedAvailability.totalSize - updatedAvailability.freeSize
-    let totalSizeResponse = (
-      await host.patchAvailabilityRaw(
-        availability.id, totalSize = (utilizedSize - 1).some
-      )
-    )
-    check totalSizeResponse.status == 422
-    check "totalSize must be larger then current totalSize" in
-      (await totalSizeResponse.body)
-
-    await host.patchAvailability(
-      availability.id, totalSize = (originalSize + 20000).some
-    )
-    let newUpdatedAvailability =
-      ((await host.getAvailabilities()).get).findItem(availability).get
-    check newUpdatedAvailability.totalSize == originalSize + 20000
-    check newUpdatedAvailability.freeSize - updatedAvailability.freeSize == 20000
-
-  test "updating availability fails with until negative", salesConfig:
-    let availability = (
-      await host.postAvailability(
-        totalSize = 140000.uint64,
-        duration = 200.uint64,
-        minPricePerBytePerSecond = 3.u256,
-        totalCollateral = 300.u256,
-      )
-    ).get
-
-    let response =
-      await host.patchAvailabilityRaw(availability.id, until = -1.SecondsSince1970.some)
-
-    check:
-      (await response.body) == "Cannot set until to a negative value"
-
-  test "returns an error when trying to update the until date before an existing a request is finished",
-    salesConfig:
-    let size = 0xFFFFFF.uint64
-    let data = await RandomChunker.example(blocks = 8)
-    let duration = 20 * 60.uint64
-    let minPricePerBytePerSecond = 3.u256
-    let collateralPerByte = 1.u256
-    let ecNodes = 3.uint
-    let ecTolerance = 1.uint
-
-    # host makes storage available
-    let availability = (
-      await host.postAvailability(
-        totalSize = size,
-        duration = duration,
-        minPricePerBytePerSecond = minPricePerBytePerSecond,
-        totalCollateral = size.u256 * minPricePerBytePerSecond,
-      )
-    ).get
-
-    # client requests storage
-    let cid = (await client.upload(data)).get
-    let id = (
-      await client.requestStorage(
-        cid,
-        duration = duration,
-        pricePerBytePerSecond = minPricePerBytePerSecond,
-        proofProbability = 3.u256,
-        expiry = 10 * 60.uint64,
-        collateralPerByte = collateralPerByte,
-        nodes = ecNodes,
-        tolerance = ecTolerance,
-      )
-    ).get
-
-    discard await waitForRequestToStart()
-
-    let purchase = (await client.getPurchase(id)).get
-    check purchase.error == none string
-
-    let unixNow = getTime().toUnix()
-    let until = unixNow + 1.SecondsSince1970
-
-    let response = await host.patchAvailabilityRaw(
-      availabilityId = availability.id, until = until.some
-    )
-
-    check:
-      response.status == 422
-      (await response.body) ==
-        "Until parameter must be greater or equal to the longest currently hosted slot"
+  test "node refuses to decrease 'until' below request end":
+    let original = await testbed.availability.create(provider)
+    let id = original["id"].getStr()
+    discard await testbed.request.start(node)
+    await sleepAsync(chronos.seconds(1))
+    try:
+      let until = (await testbed.time.now()) + 1
+      await testbed.availability.until(until).update(provider, id)
+      fail()
+    except HttpError as error:
+      check "422" in error.msg
+      check "greater or equal to the longest currently hosted slot" in error.msg
