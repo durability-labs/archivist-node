d25ceab9665c21560b094675622108435536336e
Mark Spanbroek <mark@spanbroek.net>
2025-09-18 11:48:29 +0200

    refactor(integration): remove old integration test helpers

--- tests/integration/nodeconfigs.nim
+++
@@ -1,9 +0,0 @@
-import pkg/questionable
-import ./archivistconfig
-import ./hardhatconfig
-
-type NodeConfigs* = object
-  clients*: ?ArchivistConfigs
-  providers*: ?ArchivistConfigs
-  validators*: ?ArchivistConfigs
-  hardhat*: ?HardhatConfig
