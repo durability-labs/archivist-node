2e9338de22526d394f3c87fc8ea897f14b408a91
Mark Spanbroek <mark@spanbroek.net>
2025-09-18 11:48:29 +0200

    refactor(integration): remove old integration test helpers

--- tests/integration/clioption.nim
+++
@@ -1,9 +0,0 @@
-type CliOption* = object
-  key*: string # option key, including `--`
-  value*: string # option value
-
-proc `$`*(option: CliOption): string =
-  var res = option.key
-  if option.value.len > 0:
-    res &= "=" & option.value
-  return res
