3adaeb2e8baf2bed435918f067f75a6117332755
Mark Spanbroek <mark@spanbroek.net>
2025-10-21 08:01:57 +0000

    fix(integration): stop testbed at end of test

--- tests/integration/1_minute/testecbug.nim
+++ tests/integration/1_minute/testecbug.nim
@@ -13,6 +13,9 @@ suite "Bug 821 - node crashes during erasure coding":
     discard await testbed.hardhat.start()
     node = await testbed.node.persistence.start()
 
+  teardown:
+    await testbed.stop()
+
   test "should be able to create storage request and download dataset":
     let request = await testbed.request.submit(node)
     let purchase = await testbed.api(node).getPurchase(request.id)
