71c21df4712978cc47c67d9b1c668b7aaeeaeacc
Slava <20563034+veaceslavdoina@users.noreply.github.com>
2025-11-05 14:24:43 +0200

    chore(docker): switch to a config endpoint (#40)
    * chore(docker): switch to a config endpoint for bootstrap nodes sprs
    
    * chore(docker): switch to a config endpoint for marketplace address
    
    * chore(docker): spelling

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,9 +10,10 @@ if [[ -n "${ENV_PATH}" ]]; then
 fi
 
 # Network
+CONFIG_URL="${CONFIG_URL:-https://config.archivist.storage}"
+BOOTSTRAP_NODE_FROM_URL="${BOOTSTRAP_NODE_FROM_URL}"
 if [[ -n "${NETWORK}" ]]; then
-  BOOTSTRAP_SERVICE_URL="https://spr.archivist.storage"
-  export BOOTSTRAP_NODE_FROM_URL="${BOOTSTRAP_NODE_FROM_URL:-${BOOTSTRAP_SERVICE_URL}/${NETWORK}}"
+  BOOTSTRAP_NODE_FROM_CONFIG_URL="${BOOTSTRAP_NODE_FROM_CONFIG_URL:-${CONFIG_URL}/${NETWORK}.json}"
 fi
 
 # Bootstrap node URL
@@ -36,11 +37,35 @@ if [[ -n "${BOOTSTRAP_NODE_URL}" ]]; then
   done
 fi
 
+# Bootstrap node from config URL
+if [[ -n "${BOOTSTRAP_NODE_FROM_CONFIG_URL}" ]]; then
+  WAIT=${BOOTSTRAP_NODE_FROM_CONFIG_URL_WAIT:-300}
+  SECONDS=0
+  SLEEP=1
+  # Run and retry if fail
+  while (( SECONDS < WAIT )); do
+    SPR=($(curl -s -f -m 5 "${BOOTSTRAP_NODE_FROM_CONFIG_URL}" | jq -r '.sprs[].records[]'))
+    # Check if exit code is 0 and returned value is not empty
+    if [[ $? -eq 0 && -n "${SPR}" ]]; then
+      for node in "${SPR[@]}"; do
+        bootstrap+="--bootstrap-node=$node "
+      done
+      set -- "$@" ${bootstrap}
+      break
+    else
+      # Sleep and check again
+      echo "Can't get SPR from ${BOOTSTRAP_NODE_FROM_CONFIG_URL} - Retry in $SLEEP seconds / $((WAIT - SECONDS))"
+      sleep $SLEEP
+    fi
+  done
+fi
+
 # Bootstrap node from URL
 if [[ -n "${BOOTSTRAP_NODE_FROM_URL}" ]]; then
   WAIT=${BOOTSTRAP_NODE_FROM_URL_WAIT:-300}
   SECONDS=0
   SLEEP=1
+  echo -e "\nDeprecation notice: BOOTSTRAP_NODE_FROM_URL variable is deprecated, please switch to a BOOTSTRAP_NODE_FROM_CONFIG_URL\n"
   # Run and retry if fail
   while (( SECONDS < WAIT )); do
     SPR=($(curl -s -f -m 5 "${BOOTSTRAP_NODE_FROM_URL}"))
@@ -59,11 +84,32 @@ if [[ -n "${BOOTSTRAP_NODE_FROM_URL}" ]]; then
   done
 fi
 
+# Marketplace address from config URL
+if [[ -n "${MARKETPLACE_ADDRESS_FROM_CONFIG_URL}" ]]; then
+  WAIT=${MARKETPLACE_ADDRESS_FROM_CONFIG_URL_WAIT:-300}
+  SECONDS=0
+  SLEEP=1
+  # Run and retry if fail
+  while (( SECONDS < WAIT )); do
+    MARKETPLACE_ADDRESS=($(curl -s -f -m 5 "${MARKETPLACE_ADDRESS_FROM_CONFIG_URL}" | jq -r '.marketplace[].contractAddress'))
+    # Check if exit code is 0 and returned value is not empty
+    if [[ $? -eq 0 && -n "${MARKETPLACE_ADDRESS}" ]]; then
+      export ARCHIVIST_MARKETPLACE_ADDRESS="${MARKETPLACE_ADDRESS}"
+      break
+    else
+      # Sleep and check again
+      echo "Can't get Marketplace address from ${MARKETPLACE_ADDRESS_FROM_CONFIG_URL} - Retry in $SLEEP seconds / $((WAIT - SECONDS))"
+      sleep $SLEEP
+    fi
+  done
+fi
+
 # Marketplace address from URL
 if [[ -n "${MARKETPLACE_ADDRESS_FROM_URL}" ]]; then
   WAIT=${MARKETPLACE_ADDRESS_FROM_URL_WAIT:-300}
   SECONDS=0
   SLEEP=1
+  echo -e "\nDeprecation notice: MARKETPLACE_ADDRESS_FROM_URL variable is deprecated, please switch to a MARKETPLACE_ADDRESS_FROM_CONFIG_URL\n"
   # Run and retry if fail
   while (( SECONDS < WAIT )); do
     MARKETPLACE_ADDRESS=($(curl -s -f -m 5 "${MARKETPLACE_ADDRESS_FROM_URL}"))
@@ -157,7 +203,7 @@ if [[ "$@" == *"prover"* ]]; then
 fi
 
 # Show
-echo -e "\nnode run parameters:"
+echo -e "\nNode run parameters:"
 vars=$(env | grep "ARCHIVIST_" | grep -v -e "[0-9]_SERVICE_" -e "[0-9]_NODEPORT_")
 echo -e "${vars//ARCHIVIST_/   - ARCHIVIST_}"
 echo -e "   - $@\n"
