951babed0f480e6960a9cf06ea3f1fe71e5dae2c
Ben <thatbenbierens@gmail.com>
2025-11-25 13:37:21 +0100

    Implements missing waitUntil on system clock

--- archivist/systemclock.nim
+++ archivist/systemclock.nim
@@ -1,4 +1,5 @@
 import std/times
+import pkg/chronos
 import ./clock
 
 type SystemClock* = ref object of Clock
@@ -6,3 +7,8 @@ type SystemClock* = ref object of Clock
 method now*(clock: SystemClock): SecondsSince1970 {.raises: [].} =
   let now = times.now().utc
   now.toTime().toUnix()
+
+method waitUntil*(clock: SystemClock, time: SecondsSince1970) {.async.} =
+  let toWait = time - clock.now()
+  if toWait > 0:
+    await sleepAsync(toWait.seconds)
