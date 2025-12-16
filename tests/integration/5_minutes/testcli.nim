f6f68b42d2028256b6903ed2cd3667d761e6a6a5
Mark Spanbroek <mark@spanbroek.net>
2025-12-16 12:01:05 +0100

    fix(cli): fix merge problem in test

--- tests/integration/5_minutes/testcli.nim
+++ tests/integration/5_minutes/testcli.nim
@@ -88,7 +88,7 @@ suite "Command line interface":
 
   test "automatically uses local config.toml":
     let
-      expectedOutput = "Persistence enabled, but no Ethereum account was set"
+      expectedOutput = "Persistence enabled, but no Ethereum private key was set"
       configFile = "config.toml"
       content = "persistence=true"
 
