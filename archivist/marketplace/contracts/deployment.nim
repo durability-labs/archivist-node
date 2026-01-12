3bb8c9384a1b75eec0e7e2d62622e6b89e5f675c
ThatBen <39762930+benbierens@users.noreply.github.com>
2026-01-12 10:28:18 +0100

    chore: testnet marketplace address updated [release] (#86)

--- archivist/marketplace/contracts/deployment.nim
+++ archivist/marketplace/contracts/deployment.nim
@@ -8,8 +8,8 @@ const MarketplaceAddresses = {
   "31337": !Address.init("0x322813Fd9A801c5507c9de605d63CEA4f2CE6c44"),
   # Taiko Alpha-3 Testnet
   "167005": !Address.init("0x948CF9291b77Bd7ad84781b9047129Addf1b894F"),
-  # testnet - 2025-11-14 08:21:08 - UTC
-  "421614": !Address.init("0x68d043b20C0b6aBF9932357927C5032Cc916470d"),
+  # testnet - 2026-01-12 09:14:08 - UTC
+  "421614": !Address.init("0x9A110Ae7DC8916Fa741e38caAf204c3ace3eAB0c"),
   # Linea (Status)
   "1660990954": !Address.init("0x34F606C65869277f236ce07aBe9af0B8c88F486B"),
 }.toTable
