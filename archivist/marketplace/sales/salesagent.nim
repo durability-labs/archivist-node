codex-storage/nim-codex

contracts: introduce types for time and tokens
fixes bugs with slotqueue and repostore, where
expiry duration was misinterpreted as expiry
timestamp

codex/sales/salesagent.nim
@@ -28,7 +28,7 @@ type
     onFilled*: ?OnFilled
 
   OnCleanUp* = proc(
-    returnBytes = false, reprocessSlot = false, returnedCollateral = UInt256.none
+    returnBytes = false, reprocessSlot = false, returnedCollateral = Tokens.none
   ): Future[void] {.gcsafe, upraises: [].}
   OnFilled* = proc(request: StorageRequest, slotIndex: uint64) {.gcsafe, upraises: [].}
 
@@ -82,7 +82,7 @@ proc subscribeCancellation(agent: SalesAgent) {.async.} =
 
     try:
       let market = agent.context.market
-      let expiry = await market.requestExpiresAt(data.requestId)
+      let expiry = (await market.requestExpiresAt(data.requestId)).toSecondsSince1970
 
       while true:
         let deadline = max(clock.now, expiry) + 1

durability-labs/archivist-node

archivist/marketplace/sales/salesagent.nim
@@ -24,7 +24,7 @@ type
     onCleanUp*: OnCleanUp
     onFilled*: ?OnFilled
 
-  OnCleanUp* = proc(reprocessSlot = false, returnedCollateral = UInt256.none) {.
+  OnCleanUp* = proc(reprocessSlot = false, returnedCollateral = Tokens.none) {.
     async: (raises: [])
   .}
   OnFilled* = proc(request: StorageRequest, slotIndex: uint64) {.gcsafe, raises: [].}
@@ -79,7 +79,7 @@ proc subscribeCancellation(agent: SalesAgent) {.async.} =
 
     try:
       let marketplace = agent.context.marketplace
-      let expiry = await marketplace.requestExpiresAt(data.requestId)
+      let expiry = (await marketplace.requestExpiresAt(data.requestId)).toSecondsSince1970
 
       while true:
         let deadline = max(clock.now, expiry) + 1