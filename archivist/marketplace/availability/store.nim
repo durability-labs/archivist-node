07f9985ac8085abc86446c35dda9536c263e37bb
Dmitriy Ryajov <dryajov@gmail.com>
2026-02-13 22:19:33 -0600

    feat: proper upsert for availability

--- archivist/marketplace/availability/store.nim
+++ archivist/marketplace/availability/store.nim
@@ -16,10 +16,22 @@ func new*(_: type AvailabilityStore, store: KVStore): AvailabilityStore =
 proc load*(
     availability: AvailabilityStore
 ): Future[?!AvailabilityTerms] {.async: (raises: [CancelledError]).} =
-  let record = ?await get(availability.store, DataStoreKey, AvailabilityTerms)
+  let record = ?await get(availability.store, DatastoreKey, AvailabilityTerms)
   success(record.val)
 
 proc save*(
     availability: AvailabilityStore, terms: AvailabilityTerms
 ): Future[?!void] {.async: (raises: [CancelledError]).} =
-  await availability.store.put(DataStoreKey, terms)
+  ?await availability.store.tryPut(
+    KVRecord[AvailabilityTerms].init(DatastoreKey, terms),
+    maxRetries = 3,
+    proc(
+        failed: seq[KVRecord[AvailabilityTerms]]
+    ): Future[?!seq[KVRecord[AvailabilityTerms]]] {.async: (raises: [CancelledError]).} =
+      var record = ?await availability.store.get(failed[0].key, AvailabilityTerms)
+      record.val = terms
+      success @[record]
+    ,
+  )
+
+  success()
