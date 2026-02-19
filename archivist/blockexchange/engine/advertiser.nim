9584873090bc5679262264f79419dac47a3ebfac
Dmitriy Ryajov <dryajov@gmail.com>
2026-02-19 15:17:45 -0600

    feat: dispose listBlocks iterator in discovery

--- archivist/blockexchange/engine/advertiser.nim
+++ archivist/blockexchange/engine/advertiser.nim
@@ -83,11 +83,20 @@ proc advertiseBlock(b: Advertiser, cid: Cid) {.async: (raises: [CancelledError])
 proc advertiseLocalStoreLoop(b: Advertiser) {.async: (raises: []).} =
   try:
     while b.advertiserRunning:
-      if cidsIter =? await b.localStore.listBlocks(blockType = BlockType.Manifest):
-        trace "Advertiser begins iterating blocks..."
-        for c in cidsIter:
-          if cid =? await c:
-            await b.advertiseBlock(cid)
+      without cidsIter =? await b.localStore.listBlocks(blockType = BlockType.Manifest),
+        err:
+        trace "Error retriving manifest iterator, adverticing skipped!", err = err.msg
+        await sleepAsync(b.advertiseLocalStoreLoopSleep)
+        continue
+
+      defer:
+        if err =? (await cidsIter.dispose()).errorOption:
+          warn "Error disposing overlay iterator", err = err.msg
+
+      trace "Advertiser begins iterating blocks..."
+      for c in cidsIter:
+        if cid =? await c:
+          await b.advertiseBlock(cid)
         trace "Advertiser iterating blocks finished."
 
       await sleepAsync(b.advertiseLocalStoreLoopSleep)
