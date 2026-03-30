ec62d5ca41ca3ea6c66354f77a6cdbd80beb8199
Mark Spanbroek <mark@spanbroek.net>
2026-03-30 11:59:35 +0200

    chore(nat): add logging

--- archivist/nat/traversal.nim
+++ archivist/nat/traversal.nim
@@ -5,9 +5,13 @@ import pkg/chronos
 import pkg/chronos/threadsync
 import pkg/taskpools
 import pkg/libp2p/multiaddress
+import pkg/chronicles
 import ../errors
 import ./config
 import ./portmapping
+
+logScope:
+  topics = "nat"
 
 type
   NatTraversal* = ref object
@@ -20,16 +24,19 @@ type
   OnPortsMapped* = proc(addresses: seq[MultiAddress]) {.gcsafe, raises: [].}
 
 proc new*(_: type NatTraversal, config: NatConfig, taskpool: TaskPool): ?!NatTraversal =
-  success NatTraversal(portmapping: ?PortMapping.init(config), taskpool: taskpool)
+  let portmapping = ?PortMapping.init(config)
+  success NatTraversal(portmapping: portmapping, taskpool: taskpool)
 
 proc renew(traversal: NatTraversal) {.async: (raises: []).}
 
 proc start*(traversal: NatTraversal) {.async: (raises: [CancelledError]).} =
   traversal.renewing = traversal.renew()
+  debug "started nat traversal"
 
 proc stop*(traversal: NatTraversal) {.async: (raises: []).} =
   await traversal.renewing.cancelAndWait()
   traversal.taskPool.shutdown()
+  debug "stopped nat traversal"
 
 proc mapTask(
     portmapping: ptr PortMapping,
@@ -37,7 +44,9 @@ proc mapTask(
     signal: ThreadSignalPtr,
     result: ptr [?!MultiAddress],
 ) =
+  trace "started port mapping background task", address
   result[] = portmapping[].map(address)
+  trace "finished port mapping background task", address, result = result[]
   discard signal.fireSync()
 
 proc map(
@@ -47,6 +56,7 @@ proc map(
     return failure "unable to create thread signal"
   let portmapping = addr traversal.portmapping
   var mapped: ?!MultiAddress
+  trace "spawning port mapping background task", address
   traversal.taskpool.spawn mapTask(portmapping, address, signal, addr mapped)
   if error =? catchAsync(await signal.wait()).errorOption:
     discard signal.close()
@@ -59,23 +69,35 @@ proc map(
     traversal: NatTraversal, addresses: seq[MultiAddress]
 ): Future[seq[MultiAddress]] {.async: (raises: [CancelledError]).} =
   for address in addresses:
-    if mapped =? await traversal.map(address):
-      result.add(mapped)
+    without mapped =? await traversal.map(address), error:
+      warn "port mapping failed", address, error = error.msg
+      continue
+    result.add(mapped)
 
 proc map*(
     traversal: NatTraversal, addresses: seq[MultiAddress], callback: OnPortsMapped
 ) {.async: (raises: [CancelledError]).} =
+  debug "mapping ports", addresses
   let mapped = await traversal.map(addresses)
+  debug "mapping ports done", addresses, mapped
   if not (previous =? traversal.mappings .? [addresses]) or previous != mapped:
+    trace "port mappings changed, notifying callback"
     traversal.mappings[addresses] = mapped
     callback(mapped)
+  else:
+    trace "port mappings remain unchanged"
 
 proc renew(traversal: NatTraversal) {.async: (raises: []).} =
   try:
     while true:
       let interval = traversal.portmapping.portMappingLifetime div 3
+      trace "waiting before renewing port mappings", interval
       await sleepAsync(interval)
+      trace "renewing port mappings"
       for (addresses, callback) in traversal.callbacks.pairs:
+        trace "renewing port mappings", addresses
         await traversal.map(addresses, callback)
+      trace "finished renewing port mappings"
   except CancelledError:
+    debug "port mapping renewal stopped"
     discard
