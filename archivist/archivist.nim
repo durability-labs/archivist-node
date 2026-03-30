ea0dbb817a4776b5c60c16fee5121c3c2ac74406
Mark Spanbroek <mark@spanbroek.net>
2026-03-30 13:34:33 +0200

    fix(nat): do not use port 0 for port mapping
    Announce port is configured as port 0, so we should take
    the actual port from libp2p, not the port from the config

--- archivist/archivist.nim
+++ archivist/archivist.nim
@@ -99,12 +99,12 @@ proc start*(s: NodeServer) {.async.} =
 
   await s.natTraversal.start()
 
-  let discoveryAddress =
-    MultiAddress.init(IPv4_any(), udpProtocol, s.config.discoveryPort)
+  let announceAddresses = s.archivistNode.switch.peerInfo.addrs
+  let discoveryPort = s.config.discoveryPort
+  let discoveryAddress = MultiAddress.init(IPv4_any(), udpProtocol, discoveryPort)
   await s.natTraversal.mapPorts(@[discoveryAddress]) do(mapped: seq[MultiAddress]):
     s.archivistNode.discovery.updateDhtRecord(mapped)
-
-  await s.natTraversal.mapPorts(s.config.listenAddrs) do(mapped: seq[MultiAddress]):
+  await s.natTraversal.mapPorts(announceAddresses) do(mapped: seq[MultiAddress]):
     s.archivistNode.discovery.updateAnnounceRecord(mapped)
 
   await s.connectMarketplace()
