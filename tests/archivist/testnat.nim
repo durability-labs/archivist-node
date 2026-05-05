8c49fa9ab55b8bff21c527f7b3806dc339a327fd
Dmitriy Ryajov <dryajov@gmail.com>
2026-05-05 09:17:18 -0600

    feat: add no nat tests (#128)

--- tests/archivist/testnat.nim
+++ tests/archivist/testnat.nim
@@ -7,16 +7,68 @@ import pkg/archivist/nat
 import ../asynctest
 
 suite "NAT Traversal":
-  let config = NatConfig.externalIp(parseIpAddress("8.8.8.8"))
+  let externalConfig = NatConfig.externalIp(parseIpAddress("8.8.8.8"))
+  let noNatConfig = NatConfig.noNat()
 
   var nat: NatTraversal
 
   setup:
-    nat = NatTraversal.new(config, 20.minutes, TaskPool.new(2))
+    nat = NatTraversal.new(externalConfig, 20.minutes, TaskPool.new(2))
     await nat.start()
 
   teardown:
     await nat.stop()
+
+  test "discovery addresses derive UDP multiaddrs from direct IPs":
+    let announceAddresses =
+      @[
+        MultiAddress.init("/ip4/10.0.0.1/tcp/4000").get(),
+        MultiAddress.init("/ip4/10.0.0.1/tcp/5000").get(),
+        MultiAddress.init("/ip6/2001:db8::1/tcp/4000").get(),
+        MultiAddress.init("/dns4/example.com/tcp/4000").get(),
+      ]
+
+    let expected =
+      @[
+        MultiAddress.init("/ip4/10.0.0.1/udp/30303").get(),
+        MultiAddress.init("/ip6/2001:db8::1/udp/30303").get(),
+      ]
+
+    check discoveryAddresses(announceAddresses, Port(30303)) == expected
+
+  test "no nat passes mapped addresses through unchanged":
+    let addresses =
+      @[
+        MultiAddress.init("/ip4/127.0.0.1/tcp/5000").get(),
+        MultiAddress.init("/ip4/0.0.0.0/tcp/5000").get(),
+        MultiAddress.init("/ip4/192.168.1.1/tcp/5000").get(),
+        MultiAddress.init("/ip6/::1/tcp/5000").get(),
+        MultiAddress.init("/ip4/0.0.0.0/udp/1234").get(),
+        MultiAddress.init("/ip6/::/udp/1234").get(),
+        MultiAddress.init("/ip4/127.0.0.1/tcp/5000").get(),
+      ]
+
+    let expected =
+      @[
+        MultiAddress.init("/ip4/127.0.0.1/tcp/5000").get(),
+        MultiAddress.init("/ip4/0.0.0.0/tcp/5000").get(),
+        MultiAddress.init("/ip4/192.168.1.1/tcp/5000").get(),
+        MultiAddress.init("/ip6/::1/tcp/5000").get(),
+        MultiAddress.init("/ip4/0.0.0.0/udp/1234").get(),
+        MultiAddress.init("/ip6/::/udp/1234").get(),
+      ]
+
+    var noNatTraversal = NatTraversal.new(noNatConfig, 20.minutes, TaskPool.new(2))
+    await noNatTraversal.start()
+    defer:
+      await noNatTraversal.stop()
+
+    var mapped: seq[MultiAddress]
+
+    await noNatTraversal.mapPorts(addresses) do(result: seq[MultiAddress]):
+      mapped = result
+
+    check mapped == expected
 
   test "replaces internal IP address with external IP address":
     let addresses =
